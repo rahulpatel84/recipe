cb685419b3cfcead7ca60e9d3a480246e9561fd8
Rahul <rahul@Rahuls-MacBook-Pro.local>
2026-02-14 22:15:01 +0000

    add favicon using relish-recipe.png

--- recipe-app/app/layout.tsx
+++ recipe-app/app/layout.tsx
@@ -69,9 +69,7 @@ export const metadata: Metadata = {
     canonical: siteConfig.url,
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: "/icon.png",
   },
   manifest: "/site.webmanifest",
 };
